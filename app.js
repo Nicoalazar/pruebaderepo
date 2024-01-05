860d46bd9f6dd5928c70101c88494ad0a39e125f
Nicoalazar <nicozalazar67@gmail.com>
2024-01-05 13:32:15 -0300

    Segundo  guardado

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ console.log(saludarMundo())
 
 console.warn('Ocurrio un error...')
 
-console.error(new Error('Ocurrio un error'))
+console.error(new Error('Ocurrio un error en actualizar'))
 
 console.log(process.argv)
